2a6ca85583a35e60767fe27947952b538ccc988f
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-23 14:48:00 +0100

    Test

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>About how we do our reviews.</p>
+    </>
+  );
+}
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+  return (
+    <>
+      <h1>Hollow Knight</h1>
+      <p>Here is my review about Hollow Knight</p>
+    </>
+  );
+}
