74ee4e93cf954a156842bc310a0649ffb4ce1c2d
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-23 15:21:20 +0100

    package version number

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header>[header]</header>
-      <main>{children}</main>
-      <footer>[footer]</footer>
+      <body>
+        <header>[header]</header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 export default function HomePage() {
+  // console.log('[HomePage] Rendering!');
   return (
     <>
-      <h1>Indie Gamer</h1>
+      <h1>Indie Games</h1>
       <p>Only the bast indie games, reviewed for you</p>
     </>
   );
 }
-
-// http://localhost:3000/
