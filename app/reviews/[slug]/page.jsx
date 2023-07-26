87b114e8d198e964b7ff4a916312ced6c3c552d4
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-26 14:24:29 +0100

    Dynamic routes and rendering them

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,15 +1,16 @@
 import Heading from '@/components/Heading';
 import { getReview } from '@/lib/reviews';
 
-export default async function StardewValleyPage() {
-  const review = await getReview('stardew-valley');
+export default async function ReviewPage({ params: { slug } }) {
+  //console.log('[RevewPage] props:', props);
+  const review = await getReview(slug);
   return (
     <>
       <Heading>{review.title}</Heading>
       <p className="italic pb-2">{review.date}</p>
       <img
         src={review.image}
-        alt="hollow-knight"
+        alt=""
         width="640"
         height="360"
         className="rounded mb-2"
--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,39 +1,33 @@
 import Link from 'next/link';
 import Heading from '@/components/Heading';
+import { getReviews } from '@/lib/reviews';
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
+  //console.log('[Reviews Page] reviews:', reviews);
   return (
     <>
       <Heading>Reviews </Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-          <Link href="/reviews/hollow-knight">
-            <img
-              src="/images/hollow-knight.jpg"
-              alt="hollow-knight"
-              width="320"
-              height="180"
-              className="rounded-t mb-2"
-            />
-            <h2 className="font-semibold font-orbitron text-center py-1">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-          <Link href="/reviews/stardew-valley">
-            <img
-              src="/images/stardew-valley.jpg"
-              alt="stardew-valley"
-              width="320"
-              height="180"
-              className="rounded-t mb-2"
-            />
-            <h2 className=" font-semibold font-orbitron text-center py-1">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map(review => (
+          <li
+            key={review.slug}
+            className="border w-80 bg-white rounded shadow hover:shadow-xl"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt="hollow-knight"
+                width="320"
+                height="180"
+                className="rounded-t mb-2"
+              />
+              <h2 className="font-semibold font-orbitron text-center py-1">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
